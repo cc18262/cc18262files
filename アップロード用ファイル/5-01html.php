d1c85c8a98cbd7eb28014fdbfe037441e9d6381d
cc18262 <72294111+cc18262@users.noreply.github.com>
2020-11-03 18:23:13 +0900

    Add files via upload

---
+++ アップロード用ファイル/5-01html.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_5-01</title>
+</head>
+<body>
+    <form action="" method="post" >
+        投稿フォーム<br>
+        名前<input type="text" name="name" value=""><br>
+        コメント<input type="text" name="comment" value="" >
+        パスワード<input type="text" name="pas">
+        <input type="submit" name="submit" ><br>
+        削除フォーム<br>
+        削除番号<input type="text" name="delete_num"  >
+        パスワード<input type="text" name="delete_pas">
+        <input type="submit" value ="削除"><br>
+        編集番号指定用フォーム<br>
+        編集投稿番号<input type="text" name="direct_num"  ><br>
+        編集名前<input type="text" name="direct_name" value="">
+        編集コメント<input type="text" name="direct_comment" value="" >
+        パスワード<input type="text" name="direct_pas">
+        <input type="submit" value ="編集"><br>
+    </form>
+   
+    
+    
+    
+</body>
+</html>
